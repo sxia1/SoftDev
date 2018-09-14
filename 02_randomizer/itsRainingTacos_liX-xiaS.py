0b0cc56bf47c5f8f0bbc4982a70202dee94111fd
Sophia Xia <sxia1@stuy.edu>
2018-09-14 18:42:35 -0400

    add 02_randomizer

---
+++ 02_randomizer/itsRainingTacos_liX-xiaS.py
@@ -0,0 +1,20 @@
+# ItsRainingTacos
+# Xiaojie(Aaron) Li, Sophia Xia
+# SoftDev1 pd6
+# K02 -- NO-body expects the Spanish Inquisition!
+# 2018-09-07
+
+import random
+
+names = {
+    'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+    'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+    'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+}
+
+def theChosenOne():
+    output = random.choice(names[random.choice(list(names))])
+    print(output)
+
+theChosenOne()
+    
