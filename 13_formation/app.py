8ee93fcf215c40fa20f1abdc9e67ccb08e112401
Sophia Xia <sxia1@stuy.edu>
2018-09-28 00:44:09 -0400

    added k13

---
+++ 13_formation/app.py
@@ -0,0 +1,32 @@
+'''
+Sophia Xia
+SoftDev1 pd6
+K13 -- Echo Echo Echo
+2018-09-27
+'''
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # prints <Flask 'app'> 
+    print(app)
+    return render_template("auth.html")
+
+@app.route("/auth")
+def authenticate():
+    print(app)
+    print(request)
+    print(request.args)
+    print(request.args['username'])
+    print(request.headers)
+    # return "I like watermelon"
+    return render_template("success.html", username = request.form['username'], sub1 = request.args['sub1'])
+    
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
+app.debug = True
+app.run()
