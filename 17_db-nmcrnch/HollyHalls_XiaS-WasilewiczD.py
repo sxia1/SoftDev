9719a158bb8c31a7c844323d172abf4fd789f5fd
Sophia Xia <sxia1@stuy.edu>
2018-10-07 22:17:28 -0400

    working version

--- 17_db-nmcrnch/HollyHalls_XiaS-WasilewiczD.py
+++ 17_db-nmcrnch/HollyHalls_XiaS-WasilewiczD.py
@@ -1,3 +1,9 @@
+#Holly Halls
+#Damian Wasilewicz, Sophia Xia
+#SoftDev1 pd1
+#K17 -- Average, ... or Basic?
+#2018-10-07
+
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 import os        #for deleting discobandit
@@ -51,33 +57,34 @@ with open('data/courses.csv', 'r') as file:
 osis_num = 1;
 
 #get grades according to osis number
-def get_grade():
+def get_grade(osis_num):
     comm_grade = "SELECT mark FROM classes WHERE osis ="
     comm_grade += str(osis_num)
-    return c.execute(comm_grade)
-print get_grade().fetchone()[0]
+    return c.execute(comm_grade).fetchall()
+print get_grade(osis_num)
 
-def get_length():
+#returns how many grades a student has
+def get_length(osis_num):
     comm_length = "SELECT count(*) FROM classes WHERE osis ="
     comm_length += str(osis_num)
     return c.execute(comm_length).fetchone()[0]
+print get_length(osis_num)
 
-print get_length()
-
-#============= I AM HERE ===================================================
-
-# ISSUES WITH FETCHONE() AND GETGRADE
 #gets average according to osis number
-def computeAvg():
-    cur = get_grade()
-    #print cur
-    length = get_length()
-    print get_grade().fetchone()[0]
-    #return retVal/length
-
+def computeAvg(osis_num):
+    length = get_length(osis_num)
+    ctr = length -1
+    cur = get_grade(osis_num)
+    retVal = 0
+    while ctr >= 0:
+        retVal += int(cur[ctr][0])
+        ctr -= 1
+    return retVal/length
+print computeAvg(osis_num)
+    
 #Create table for averages
 comm_avg = """
-    CREATE TABLE IF NOT EXISTS avgs(
+    CREATE TABLE IF NOT EXISTS peeps_avgs(
         id INTEGER PRIMARY KEY,
         name TEXT,
         avg INTEGER,
@@ -87,18 +94,30 @@ c.execute(comm_avg)
 
 #Populate avg table
 while osis_num < 11:
-    comm_getname = "SELECT name FROM mypeeps WHERE osis = "
+    comm_getname = "SELECT name FROM mypeeps WHERE mypeeps.id = "
     comm_getname += str(osis_num)
-    name = c.execute(comm_getname)
-    avg = computeAvg()
-    comm_avgs = "INSERT INTO avgs(name, osis, avg) VALUES({n}{o}{a}).format(n = name[0], o = osis_num, a = avg)"
+    name = c.execute(comm_getname).fetchone()[0]
+    #print name
+    avg = computeAvg(osis_num)
+    #print avg
+    comm_pop = "INSERT INTO peeps_avgs(name, avg, osis) VALUES(\"" + name + "\", " + str(avg) + ", " + str(osis_num) + ");"
+    c.execute(comm_pop)
     osis_num += 1
 
 #display avg table
-comm_display_avg= "SELECT *FROM peeps_avg"
+comm_display_avg= "SELECT *FROM peeps_avgs"
+display = c.execute(comm_display_avg)
+ctr = 0
+while ctr < 10:
+    print display.fetchone()
+    ctr += 1
 
-#comm_avgs = "INSERT INTO avgs(name, osis, avg) VALUES('"+ 'sasha' + "', " + '3' + ", " + 'int(computeAvg())' + ");"
-#c.execute(comm_avgs)
+#add a course to the table classes
+def add_course(nname, nmark, nosis):
+    comm_add_course = "INSERT INTO classes(name, mark, osis) VALUES(\"" + nname + "\", " + str(nmark) + ", " + str(nosis) + ");"
+    c.execute(comm_add_course)
 
+add_course("IT", 100, 11)
+    
 db.commit() #save changes
 db.close()  #close database
