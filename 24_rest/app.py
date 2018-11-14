b66cf00a5969d8273ed8168eb7973228c7ce8c7f
Sophia Xia <sxia1@stuy.edu>
2018-11-13 19:40:40 -0500

    shows image and explanation

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 import urllib.request, json
 app = Flask(__name__)
 
-url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
-req = urllib.request.urlopen(url)
-data = req.read()
-encoding = req.info().get_content_charset('utf-8')
-dict = json.loads(data.decode(encoding))
-
 @app.route("/")
 def home():
-    return render_template('index.html', INFO = dict.values())
+    url = "https://api.nasa.gov/planetary/apod?api_key=zIOpt7HlAwHA2rs8CiUZvnRSUeA32xoMGsWMewtA"
+    req = urllib.request.urlopen(url)
+    data = req.read()
+    encoding = req.info().get_content_charset('utf-8')
+    dict = json.loads(data.decode(encoding))
+    return render_template('index.html', INFO = dict)
 
 if __name__ == "__main__":
     app.debug = True
