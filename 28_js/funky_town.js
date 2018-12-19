a74af600ace90b4424c337920180ded2cb3d71bb
Sophia Xia <sxia1@stuy.edu>
2018-12-18 22:01:58 -0500

    GCD

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -16,6 +16,18 @@ var fibb = function(n){
     }
 };
 
+var gcd = function (a,b) {
+    // this means the previous modulo was 0, so a divided into b
+    if (b == 0) {
+	return a
+    }
+    // finds the smallest interval between the two numbers,
+    // if that interval fully divides into b, that is the gcd
+    else {
+	return gcd(b, a % b)
+    }
+}
+
 var stulist = ["james", "tyler", "lauren", "becky", "oliver", "frank"];
 
 var randstu = function(){
